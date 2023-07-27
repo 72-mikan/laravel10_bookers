1823c9774818023bdd1e7d34a3dba36869ceaa71
72-mikan <shinzi7280.18@gmail.com>
2023-07-27 23:03:38 +0900

    book一覧部分のコンポーネント化

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -8,28 +8,9 @@
 @section('main')
   <!-- book_list -->
   <h1>Book List</h1>
-  <table>
-    <tr>
-      <th>title</th>
-      <th>body</th>
-      <th colspan="3"></th>
-    </tr>
-    @foreach($books as $book)
-    <tr>
-      <td>{{ $book->title }}</td>
-      <td>{{ $book->body }}</td>
-      <td><a href="{{ route('books.show', ['book' => $book->id]) }}">Show</a></td>
-      <td><a href="{{ route('books.edit', ['book' => $book->id]) }}">Edit</a></td>
-      <td>
-        <form method="post" action="{{ route('books.destroy', ['book' => $book->id]) }}" name="deleteForm_{{ $book->id }}">
-          @csrf
-          @method('DELETE')
-        </form>
-        <a href="javascript:document.deleteForm_{{ $book->id }}.submit()">Destroy</a>
-      </td>
-    </tr>
-    @endforeach
-  </table>
+  @include('books.components.list', [
+    'books' => $books
+  ])
 
   <!-- book_form -->
   <h1>Book Form</h1>
