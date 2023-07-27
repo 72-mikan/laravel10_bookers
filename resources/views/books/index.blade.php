a3c501d7c781ba9106fed99cbdee2084b72696a4
72-mikan <shinzi7280.18@gmail.com>
2023-07-27 23:10:02 +0900

    merge後のレイアウト修正

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -28,32 +28,4 @@
     'books' => $books
   ])
 
-  <!-- book_form -->
-  <h1>Book Form</h1>
-  <!-- post error -->
-  @if(count($errors) > 0)
-    <ul>
-      @foreach($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  @endif
-
-  <form method="post" action="{{ route('books.store') }}">
-    @csrf
-    <table>
-      <tr>
-        <td><label>title</label></td>
-        <td><input type="text" name="title" value="{{ old('title') }}" /></td>
-      </tr>
-      <tr>
-        <td>body</td>
-        <td><input type="text" name="body" value="{{ old('body') }}" /></td>
-      </tr>
-      <tr>
-        <td><input type="submit" value="送信" /></td>
-        <td></td>
-      </tr>
-    </table>
-  </form>
 @endsection
