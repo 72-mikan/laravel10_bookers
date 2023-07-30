e83ba2b972989153ad68ae14d15a89ba4e25ef16
72-mikan <shinzi7280.18@gmail.com>
2023-07-31 08:10:31 +0900

    flash message追加

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -76,7 +76,9 @@ class BookController extends Controller
         // 投稿エラーチェック
         $this->validate($request, Book::$rules);
         $book->fill($request->except('_token', '_method'))->save();
-        return redirect(route('books.show', ['book' => $book->id]));
+        return redirect(route('books.show', ['book' => $book->id]))
+        ->withInput()
+        ->with('alert', '投稿を編集しました');
     }
 
     // bookデータ削除処理
